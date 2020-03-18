9a0a5b3ca5b45297369a5a34ec3346858745fd7f
bea92 <gobbo.beatrice92@gmail.com>
2020-03-18 18:11:32 +0100

    edit file js

---
+++ main.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("myBottomnav");
+  if (x.className === "bottomnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "bottomnav";
+  }
+}
+
+$(document).ready(function(){
+  $("#myBottomnav").click(function(){
+    $("#myBottomnav .responsive").slideToggle("slow");
+  });
+});
