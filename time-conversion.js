3d4fe8fb7d95a90e2530367fdc76f80e2e0241f3
iosonosempreio <iosonosempreio@users.noreply.github.com>
2021-09-06 18:20:06 +0200

    time-conversion

--- time-conversion.js
+++ time-conversion.js
@@ -1,16 +1,19 @@
-let dt = DateTime.now();
-let localZone = dt.zoneName;
+const DateTime = luxon.DateTime;
 
-console.log(dt);
-console.log(localZone, DateTime.local().setZone(localZone).isValid);
+const dt = DateTime.now();
+const localZone = dt.zoneName;
+const defaultZone = "America/New_York";
+
+// console.log(dt);
+// console.log(localZone, DateTime.local().setZone(localZone).isValid);
 
 const zones = moment.tz.names();
 const availableZones = zones.filter(
   (zone) => DateTime.local().setZone(zone).isValid
 );
 
-console.log(zones);
-console.log(availableZones);
+// console.log(zones);
+// console.log(availableZones);
 
 availableZones.forEach((tz) => {
   const item = document.createElementNS(
@@ -19,8 +22,22 @@ availableZones.forEach((tz) => {
   );
   item.setAttribute("value", tz);
   if (tz === localZone) {
+    convertTimezones(localZone);
     item.setAttribute("selected", "selected");
   }
   item.innerHTML = tz;
   document.querySelector("select#time-zones").appendChild(item);
 });
+
+function convertTimezones(localZone) {
+	// console.log(localZone)
+	const slots = document.querySelectorAll(".converted-timezone")
+	slots.forEach(function(el){
+		const dataTime = el.getAttribute("data-time")
+		const date = DateTime.fromISO(`2021-09-27T${dataTime}:00:00`, { zone: defaultZone })
+		const convertedDate = date.setZone(localZone);
+		const formattedDate = convertedDate.toLocaleString(DateTime.TIME_SIMPLE);
+		// console.log(formattedDate);
+		el.innerHTML = formattedDate
+	})
+}
